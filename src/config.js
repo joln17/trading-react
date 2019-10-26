53e09076d221830a9f4478c950aa07e4d5231a5a
joln17 <33175263+joln17@users.noreply.github.com>
2019-10-26 22:00:48 +0200

    Change ws to wss

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     baseURL: 'https://trading-api.joln17.me',
-    wsURL: 'ws://trading-ws.joln17.me'
+    wsURL: 'wss://trading-ws.joln17.me'
     //baseURL: 'http://localhost:8334',
     //wsURL: 'ws://localhost:8301'
 };
